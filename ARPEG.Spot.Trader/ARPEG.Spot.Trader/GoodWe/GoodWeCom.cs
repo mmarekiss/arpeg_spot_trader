a2d707e6d5c41695a858a3d39712d9fbbf23015c
Martin Marek <martin.marek@artin.cz>
2023-05-16 06:00:00 +0200

    reapir SN

--- ARPEG.Spot.Trader/ARPEG.Spot.Trader/GoodWe/GoodWeCom.cs
+++ ARPEG.Spot.Trader/ARPEG.Spot.Trader/GoodWe/GoodWeCom.cs
@@ -34,7 +34,7 @@ public class GoodWeCom
     public async Task<string?> GetInverterName(IPAddress address)
     {
         _logger.LogInformation("Try check address {0}", address);
-        ushort minAddress = 512;
+        ushort minAddress = 35003;
         var reqRegisters = 8;
         var arr = new byte[]
             { 0xF7, 0x03, (byte)(minAddress >> 8), (byte)minAddress, (byte)(reqRegisters >> 8), (byte)reqRegisters };
