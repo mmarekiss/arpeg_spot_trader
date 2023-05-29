cbbb971666486f41f0817cf63be6d763986b582e
Martin Marek <martin.marek@artin.cz>
2023-05-29 17:57:39 +0200

    Avoid Negative Price

--- ARPEG.Spot.Trader/ARPEG.Spot.Trader.Integration/RunLicence.cs
+++ ARPEG.Spot.Trader/ARPEG.Spot.Trader.Integration/RunLicence.cs
@@ -5,7 +5,8 @@ public enum LicenceVersion
 {
     None = 0x00,
     Standard = 0x01,
-    Spot = 0x02
+    Spot = 0x02,
+    AvoidNegativePrice = 0x04
 }
 
 public class RunLicence
--- ARPEG.Spot.Trader/ARPEG.Spot.Trader/Backgrounds/PriceFetcher.cs
+++ ARPEG.Spot.Trader/ARPEG.Spot.Trader/Backgrounds/PriceFetcher.cs
@@ -17,8 +17,6 @@ public class PriceFetcher : BackgroundService
     private readonly Gauge gauge;
     private readonly Gauge gaugePvForecast;
 
-    private Grid GridOptions { get; set; }
-
     private readonly IGoodWeInvStore invStore;
     private readonly ILogger<PriceFetcher> logger;
     private readonly PriceService priceService;
@@ -40,6 +38,8 @@ public class PriceFetcher : BackgroundService
         gauge = Metrics.CreateGauge("Price", "Current price from OTE");
         gaugePvForecast = Metrics.CreateGauge("PV_forecast", "Solar forecast", "part");
     }
+
+    private Grid GridOptions { get; }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -70,48 +70,28 @@ public class PriceFetcher : BackgroundService
         gaugePvForecast.WithLabels("now").Set(pvForecast);
         gaugePvForecast.WithLabels("24").Set(pForecast24);
 
-        // if (GridOptions.TradeEnergy)
-        // {
-        //     var exportLimitDef = GridOptions.ExportLimit;
-        //     var exportLimit = exportLimitDef ?? 10_000;
-        //
-        //     if (price < 10)
-        //     {
-        //         exportLimit = Math.Min(exportLimit, (ushort)200);
-        //         await InvokeMethodNonSpot(g => communicator.SetExportLimit((ushort)exportLimit, g, stoppingToken));
-        //     }
-        //     else if (exportLimitDef.HasValue)
-        //     {
-        //         await InvokeMethodNonSpot(g => communicator.SetExportLimit((ushort)exportLimit, g, stoppingToken));
-        //     }
-        //     else
-        //     {
-        //         await InvokeMethodNonSpot(g => communicator.DisableExportLimit(g, stoppingToken));
-        //     }
-        //
-        //     if ((price < -10 && pvForecast < 100)
-        //         || (priceService.IsMinPriceOfNight() && !forecastService.PossibleFulfillBattery()))
-        //         await InvokeMethodNonSpot(g =>
-        //             communicator.ForceBatteryCharge(g, (ushort)GridOptions.ChargePower, stoppingToken));
-        //     else
-        //         await InvokeMethodNonSpot(g => communicator.StopForceBatteryCharge(g, stoppingToken));
-        // }
-        // else
-        // {
-        //     logger.LogInformation("Energy trading is disabled");
-        // }
-
+        await AvoidNegativePrice(price, stoppingToken);
         await HandleExternalBatteryManagement(stoppingToken);
+    }
+
+    private async Task AvoidNegativePrice(double price,
+        CancellationToken stoppingToken)
+    {
+        foreach (var gw in invStore.GoodWes.Where(g => g.Licence.HasFlag(LicenceVersion.AvoidNegativePrice)))
+            if (price < 0)
+                await communicator.SetExportLimit(0, gw, stoppingToken);
+            else
+                await communicator.DisableExportLimit(gw, stoppingToken);
     }
 
     private async Task HandleExternalBatteryManagement(CancellationToken stoppingToken)
     {
-        foreach (var gw in invStore.GoodWes.Where(g=>g.Licence.HasFlag(LicenceVersion.Spot)))
+        foreach (var gw in invStore.GoodWes.Where(g => g.Licence.HasFlag(LicenceVersion.Spot)))
         {
             var batteryManagement = await FetchBatteryCommand(gw.SN, stoppingToken);
-            switch(batteryManagement?.BatteryManagement) 
+            switch (batteryManagement?.BatteryManagement)
             {
-                case BatteryManagement.ForceCharge :
+                case BatteryManagement.ForceCharge:
                     await communicator.ForceBatteryCharge(gw, 10_000, stoppingToken);
                     break;
                 case BatteryManagement.ForceDischarge:
@@ -126,19 +106,20 @@ public class PriceFetcher : BackgroundService
 
     private async Task InvokeMethodNonSpot(Func<Definition, Task> invokeFunc)
     {
-        await Task.WhenAll(invStore.GoodWes.Where(x=>!x.Licence.HasFlag(LicenceVersion.Spot)).Select(invokeFunc));
+        await Task.WhenAll(invStore.GoodWes.Where(x => !x.Licence.HasFlag(LicenceVersion.Spot)).Select(invokeFunc));
     }
-    
+
     private async Task InvokeMethodSpot(Func<Definition, Task> invokeFunc)
     {
-        await Task.WhenAll(invStore.GoodWes.Where(x=>x.Licence.HasFlag(LicenceVersion.Spot)).Select(invokeFunc));
+        await Task.WhenAll(invStore.GoodWes.Where(x => x.Licence.HasFlag(LicenceVersion.Spot)).Select(invokeFunc));
     }
-    
-    private async Task<BatteryManagementCommands?> FetchBatteryCommand(string name, CancellationToken cancellationToken)
+
+    private async Task<BatteryManagementCommands?> FetchBatteryCommand(string name,
+        CancellationToken cancellationToken)
     {
         var client = new HttpClient();
         return await client.GetFromJsonAsync<BatteryManagementCommands>(
             $"https://arpeg-licences.azurewebsites.net/api/batterymanagement/{name}?code=avtH1HThysw7MbnrtWo7rLxipfEkPCX18jHghGClZ2deAzFuF7K0_Q=="
-            , cancellationToken: cancellationToken);
+            , cancellationToken);
     }
 }
