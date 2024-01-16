d421256f18bacc14ed8263f7334ea9e42ffc599a
Martin Marek <martin.marek@artin.cz>
2024-01-16 17:52:15 +0100

    remove container map users

--- ARPEG.Spot.Trader/ARPEG.Spot.Trader/BitOutputs/BitController.cs
+++ ARPEG.Spot.Trader/ARPEG.Spot.Trader/BitOutputs/BitController.cs
@@ -1,11 +1,8 @@
-﻿using System.Device.Gpio;
-using ARPEG.Spot.Trader.BitOutputs.Handlers;
+﻿using ARPEG.Spot.Trader.BitOutputs.Handlers;
 using ARPEG.Spot.Trader.Config;
 using ARPEG.Spot.Trader.GoodWeCommunication;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Prometheus;
-using TecoBridge.GoodWe;
+using System.Device.Gpio;
 
 namespace ARPEG.Spot.Trader.BitOutputs;
 
@@ -14,13 +11,13 @@ public class BitController<TOptions> : IBitController, IDisposable
 {
     // private readonly Gauge _gauge;
     private readonly IEnumerable<IDataValueHandler> _handlers;
+
     private readonly ILogger<BitController<TOptions>> _logger;
     private readonly TOptions _options;
 
 #if !DEBUG
     private readonly GpioController _controller;
 #endif
-
 
     public BitController(ILogger<BitController<TOptions>> logger,
         TOptions options,
@@ -32,43 +29,43 @@ public class BitController<TOptions> : IBitController, IDisposable
         // _gauge = Metrics.CreateGauge("Outputs", "Digital outputs", "output", "description", "output_"+Guid.NewGuid().ToString());
 #if !DEBUG
         _controller = new GpioController();
-        _controller.OpenPin( _options.Pin, PinMode.Output);
+        _controller.OpenPin(_options.Pin, PinMode.Output);
 #endif
     }
 
     public Task HandleDataValue(Definition inverterDefinition,
         DataValue dataValue)
     {
-        if (inverterDefinition.Sn != _options.GwSn) return Task.CompletedTask;
-        
+        if (inverterDefinition.Sn != _options.GwSn)
+            return Task.CompletedTask;
+
         var handler = _handlers.FirstOrDefault(x => x.Type == _options.DriverType);
 
-        var value = handler?.Handle(dataValue, _options );
+        var value = handler?.Handle(dataValue, _options);
 
         var description = CreateDescription(_options);
-        if(value.HasValue){
+        if (value.HasValue)
+        {
             _logger.LogDebug("Set output for pin {pinId} {value}", _options.Pin, value);
-        
+
 #if !DEBUG
-          _controller.Write(_options.Pin, !value.Value);
-#endif  
+            _controller.Write(_options.Pin, !value.Value);
+#endif
         }
-        
+
         return Task.CompletedTask;
     }
 
     private string CreateDescription(TOptions options)
     {
         var mark = _options.GreaterThen ? ">" : "<";
-        return $"{_options.DriverType} {mark} { _options.TriggerValue}";
+        return $"{_options.DriverType} {mark} {_options.TriggerValue}";
     }
 
     public void Dispose()
     {
-        
 #if !DEBUG
         _controller.Dispose();
 #endif
-        
     }
 }
